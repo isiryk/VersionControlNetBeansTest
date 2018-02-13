3bc04cb8125821dd472bb4b4c08bee6ef482017b
isiryk <siryk.ivan@gmail.com>
2018-02-13 03:07:19 -0500

    Testing part 1

---
+++ VersionControlTest/src/versioncontroltest/VersionControlTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versioncontroltest;
+
+/**
+ *
+ * @author Kepler
+ */
+public class VersionControlTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public int add(int a, int b){
+        return a + b;
+    }
+    
+    public int sub(int a, int b){
+        return a - b;
+    }
+    
+}
