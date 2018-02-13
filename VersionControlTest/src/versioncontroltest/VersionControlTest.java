7399d3b2147b3b905cf20387366003b3ae8df257
isiryk <siryk.ivan@gmail.com>
2018-02-13 03:21:39 -0500

    Testing part 3 - Fixed readline, needed new scanner

--- VersionControlTest/src/versioncontroltest/VersionControlTest.java
+++ VersionControlTest/src/versioncontroltest/VersionControlTest.java
@@ -16,20 +16,21 @@ public class VersionControlTest {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         System.out.println("Please enter 2 numbers");
         Scanner scan = new Scanner(System.in);
+        Scanner answer = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
         System.out.println("Do you want to add them or subtract " + b + " from " + a + "?(add/subtract)");
-        String response = scan.nextLine();
+        String response = answer.nextLine();
         switch(response){
         case "add":
-            System.out.print("Adding " + a + " and " + b + "yields" + add(a,b));
+            System.out.print("Adding " + a + " and " + b + " yields " + add(a,b) +".\n");
             break;
         case "subtract":
-            System.out.print("Subtracting " + b + " from " + a + "yields" + add(a,b));
+            System.out.print("Subtracting " + b + " from " + a + " yields " + add(a,b) +".\n");
             break;         
         }
     }
