b0d6a818bfb3a64ed359faf13b9f13ffd75721e7
isiryk <siryk.ivan@gmail.com>
2018-02-13 03:25:08 -0500

    Test 4 - added a loop to ask until asked to stop

--- VersionControlTest/src/versioncontroltest/VersionControlTest.java
+++ VersionControlTest/src/versioncontroltest/VersionControlTest.java
@@ -18,21 +18,38 @@ public class VersionControlTest {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        System.out.println("Please enter 2 numbers");
+        Scanner loop = new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
         Scanner answer = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        System.out.println("Do you want to add them or subtract " + b + " from " + a + "?(add/subtract)");
-        String response = answer.nextLine();
-        switch(response){
-        case "add":
-            System.out.print("Adding " + a + " and " + b + " yields " + add(a,b) +".\n");
-            break;
-        case "subtract":
-            System.out.print("Subtracting " + b + " from " + a + " yields " + add(a,b) +".\n");
-            break;         
+        boolean con = true;
+        while(con){
+            System.out.println("Please enter 2 numbers");
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println("Do you want to add them or subtract " + b + " from " + a + "?(add/subtract)");
+            String response = answer.nextLine();
+            switch(response){
+            case "add":
+                System.out.print("Adding " + a + " and " + b + " yields " + add(a,b) +".\n");
+                break;
+            case "subtract":
+                System.out.print("Subtracting " + b + " from " + a + " yields " + add(a,b) +".\n");
+                break;         
+            }
+            System.out.println("Do you wish to continue?(y/n)");
+            String cont = loop.nextLine();
+            switch(cont){
+                case "y":
+                    con = true;
+                    break;
+                case "n":
+                    con = false;
+                    break;
+            }
         }
+        loop.close();
+        scan.close();
+        answer.close();
     }
     
     public static int add(int a, int b){
